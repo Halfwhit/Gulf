6774fcc40e05db16c929a5cfbee87859adb33275
Halfwhit <Halfwhit>
2019-03-19 11:31:39 +0000

    Main scene now loads the splash/menu Tidied up file directory

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -11,3 +11,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Test_pressed():
+	pass # Replace with function body.
