d80bfa03d85630ed0ba5ff27cd6fc5abab362118
Halfwhit <Halfwhit@gmail.com>
2020-04-15 14:06:36 +0100

    Simplified the player node

--- assets/Player.gd
+++ assets/Player.gd
@@ -1,29 +1,30 @@
 extends KinematicBody2D
 
-var MAX_FORCE = 300
-var FORCE_MULTIPLIER = 3
+var MAX_FORCE = 350
+var FORCE_MULTIPLIER = 5
 var FRICTION = 0.98
 
 var ball_vector = Vector2.ZERO
-var in_motion = false
+var turn = true
 
 func _ready() -> void:
 	position = get_tree().get_root().get_node("Main/World/SpawnPoint").position
 
-func _draw():
-	if name == str(Steamworks.STEAM_ID):
-		draw_line(Vector2(), get_local_mouse_position().clamped(MAX_FORCE), Color(0.0, 0.0, 0.0), 1.0, false)
-
 func _process(delta: float) -> void:
-	update()  # Calls _draw() every frame
+	$HitLine.points[1] = get_local_mouse_position().clamped(MAX_FORCE)
+	check_motion()
+	# This is just used for testing, do this somewhere else properly
 	if Input.is_action_just_pressed("ui_cancel"):
 		print("Escape pressed, goodbye.")
 		get_tree().quit()
+	if Input.is_action_just_pressed("ui_select"):
+		turn = true
 
 func _physics_process(delta: float) -> void:
 	# Get Input
-	if Input.is_action_just_pressed("touch_main"):
+	if turn and Input.is_action_just_pressed("touch_main"):
 		ball_vector = -get_local_mouse_position().clamped(MAX_FORCE) * FORCE_MULTIPLIER
+		turn = false
 	# Handle collisions
 	var collision_info = move_and_collide(ball_vector * delta)
 	if collision_info:
@@ -34,4 +35,7 @@ func _physics_process(delta: float) -> void:
 func check_motion():
 	if ball_vector.length() <= 1: #if ball has nearly or has stopped moving
 		ball_vector = Vector2.ZERO
-		in_motion = false
+		if turn:
+			$HitLine.show()
+	else:
+		$HitLine.hide()
