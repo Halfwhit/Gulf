70b39a94beed359ee729f877bb58a249bd6c7fcc
Halfwhit <Halfwhit@gmail.com>
2020-05-19 14:29:42 +0100

    Reworked tile variants

--- scenes/TileSelector.gd
+++ scenes/TileSelector.gd
@@ -1,7 +1,7 @@
 extends PanelContainer
 
 var tile_layer
-var base_tile
+
 var selected_tile_id
 
 signal tile_selected(layer_id, id)
@@ -13,21 +13,10 @@ enum Layer {
 	EMPTY
 }
 
-func _on_BaseTile_item_selected(index: int) -> void:
+
+func ground_tile_selected(id) -> void:
 	tile_layer = Layer.GROUND
-	base_tile = index
-	selected_tile_id = base_tile * 4
-	emit_signal("tile_selected", tile_layer, selected_tile_id)
-
-
-func _on_TileVariant_item_selected(index: int) -> void:
-	tile_layer = Layer.GROUND
-	selected_tile_id = 16 + (base_tile * 8) + index
-	emit_signal("tile_selected", tile_layer, selected_tile_id)
-
-
-func _on_TileVariant_nothing_selected() -> void:
-	selected_tile_id = base_tile * 4
+	selected_tile_id = id
 	emit_signal("tile_selected", tile_layer, selected_tile_id)
 
 
