c11911fa93a3696406323e5eec1fd32da2f1f536
Halfwhit <halfwhit@gmail.com>
2026-05-12 18:17:53 +0100

    Ensured tee-fuckery works

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,30 @@
 extends RigidBody2D
 
-@export var MAX_FORCE = 350
-@export var FORCE_MULTIPLIER = 5
+@export var MAX_FORCE = 300
+@export var FORCE_MULTIPLIER = 3
 @export var FRICTION = 0.98
 
 var ball_vector = Vector2.ZERO
 var draw_line: bool = false
 var active: bool = false
 var waiting: bool = false
-
+var start_pos
 
 signal turn_taken
+
+func _ready() -> void:
+	start_pos = position
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("touch_main") && draw_line == true:
 		ball_vector = -get_local_mouse_position().limit_length(MAX_FORCE) * FORCE_MULTIPLIER
 		draw_line = false
 		waiting = true
-		print("Hit made")
+
 
 func _process(delta):
+	#if position.distance_to(start_pos) > 4:
+		#set_collision_mask_value(2, true)
 	if draw_line:
 		$HitLine.visible = true
 		$HitLine.points[1] = get_local_mouse_position().limit_length(MAX_FORCE)
@@ -49,4 +54,6 @@ func _physics_process(delta):
 func play_turn():
 	active = true
 	draw_line = true
+	freeze = false
+	$CollisionShape2D.call_deferred("set_disabled", false)
 	await(turn_taken)
